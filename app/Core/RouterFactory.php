3bcf1815370f3ce2bf93528a310832c64519eaee
Matronator <5470780+matronator@users.noreply.github.com>
2024-07-26 05:34:54 +0200

    add header, back-to-top button, modify routes, add logo

--- app/Core/RouterFactory.php
+++ app/Core/RouterFactory.php
@@ -15,6 +15,7 @@ final class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
+		$router->addRoute('collect[/<key>]', 'Home:collect');
 		$router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
 		return $router;
 	}
